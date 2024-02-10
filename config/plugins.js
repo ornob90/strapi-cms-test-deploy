73484e9f25587b6c9801bb1ea0f9f25e20455d8f
ornob90 <oornob49@gmail.com>
2024-02-10 15:14:46 +0600

    adding env

--- config/plugins.js
+++ config/plugins.js
@@ -1,28 +1,28 @@
 module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+  // ...
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-    "vercel-deploy": {
-       enabled: true,
-       config: {
-        deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
-        apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
-        appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
-        teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
-        roles: ["strapi-super-admin"],
-      }
+  },
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
+      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+      teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
+      roles: ["strapi-super-admin"],
     },
-    // ...
-  });
+  },
+  // ...
+});
